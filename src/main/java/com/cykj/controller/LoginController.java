055332ecd87e91aea206882fc709e8cea87d2e9e
myGit1-stack <924784594@qq.com>
2020-11-09 16:25:27 +0800

    第二次测试

--- src/main/java/com/cykj/controller/LoginController.java
+++ src/main/java/com/cykj/controller/LoginController.java
@@ -4,6 +4,9 @@ package com.cykj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author Administrator
+ */
 @Controller
 public class LoginController {
 
